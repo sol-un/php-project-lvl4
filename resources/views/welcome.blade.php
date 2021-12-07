2f2a387763e4a57b003f442b41726533c956409b
Andrei Soloviov <solovyev833@gmail.com>
2021-12-07 20:58:45 +0300

    Translate layouts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <div class="container-fluid py-5">
         <h1 class="display-5 fw-bold">Привет от Хекслета!</h1>
         <p class="col-md-8 fs-4">Практические курсы по программированию</p>
-        <button href="https://hexlet.io" class="btn btn-primary btn-lg" type="button">Узнать больше</button>
+        <a href="https://hexlet.io" class="btn btn-primary btn-lg" type="button">Узнать больше</a>
     </div>
 </div>
 @endsection

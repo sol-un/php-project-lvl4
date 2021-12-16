eba2747da14570a5de8c54d453b0a637e0bb46b9
Andrei Soloviov <solovyev833@gmail.com>
2021-12-16 22:30:09 +0300

    Add SCRF token

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Status;
 use \Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Log;
+
 class StatusController extends Controller
 {
     // public function __construct()

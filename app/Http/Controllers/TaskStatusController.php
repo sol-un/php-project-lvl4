351fde92bf33b116be7a9653c3da794d683c2334
Andrei Soloviov <solovyev833@gmail.com>
2022-01-23 13:25:17 +0300

    Fix linter offences

--- app/Http/Controllers/TaskStatusController.php
+++ app/Http/Controllers/TaskStatusController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\TaskStatus;
 use Illuminate\Http\Request;
 
-
 class TaskStatusController extends Controller
 {
     /**
--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -7,7 +7,6 @@ use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 use Illuminate\Support\Facades\Auth;
 
-
 class TaskPolicy
 {
     use HandlesAuthorization;
